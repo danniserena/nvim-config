ca0390c5fa2b7be5e20ed2186b520b60f5dfbbc1
danniserena <15113078698@163.com>
2024-07-02 05:32:49 +0800

    solve key mapping conflicts between nerdtree and ctags

--- lua/plugin/nerdtree/nerdtree-keyboard.lua
+++ lua/plugin/nerdtree/nerdtree-keyboard.lua
@@ -3,5 +3,7 @@ nnoremap <leader>n :NERDTreeFocus<CR>
 nnoremap <C-n> :NERDTreeCWD<CR>
 "" conflict with ctags return keys
 "" nnoremap <C-t> :NERDTreeToggle<CR>
+unmap <C-t>
+nnoremap <leader>t :NERDTreeToggle<CR>
 nnoremap <C-f> :NERDTreeFind<CR>
 ]])
